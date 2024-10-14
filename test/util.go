d73f3888d93d01e68caa73d9c48b98dd5a683148
Michal <michal83h@gmail.com>
2024-10-14 15:02:05 +0200

    v1.21.0 Adds new props to `Pipeline`

--- test/util.go
+++ test/util.go
@@ -1143,10 +1143,10 @@ func CheckPipeline(project *buddy.Project, pipeline *buddy.Pipeline, expected *b
 	if gitConfigRef == "" {
 		gitConfigRef = buddy.PipelineGitConfigRefNone
 	}
-	if gitChangeSetBase == "" && definitionSource != buddy.PipelineDefinitionSourceLocal {
+	if gitChangeSetBase == "" && definitionSource == buddy.PipelineDefinitionSourceLocal {
 		gitChangeSetBase = buddy.PipelineGitChangeSetBaseLatestRun
 	}
-	if filesystemChangeSetBase == "" && definitionSource != buddy.PipelineDefinitionSourceLocal {
+	if filesystemChangeSetBase == "" && definitionSource == buddy.PipelineDefinitionSourceLocal {
 		filesystemChangeSetBase = buddy.PipelineFilesystemChangeSetBaseDateModified
 	}
 	lenRefs := len(refs)
